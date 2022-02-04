5676eb35b5f1d6e68ad89730dccef6304f4434c1
mogata <70381096+4masa6@users.noreply.github.com>
2022-02-04 22:42:38 +0900

    15_ユーザー情報を編集できるようにする
    - php artisan make:migration add_cloumns_to_users
    - マイグレーションファイル修正
    - RegisterController修正
    - Userモデルにfillable追加
    - php artisan make:controller UserController --model=User
    - Userコントローラー編集
    - mypageアクション作成
    - edit・updateアクション修正
    - edit_addressアクション作成
    - ルーティング追加
    - 編集用ビュー作成（mypage.blade.php, edit.blade.php）
    - edit_address.blade.php作成
    - ヘッダーにマイページへのリンク作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,6 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// MyPageに関するルーティング
+Route::get('users/mypage', 'UserController@mypage')->name('mypage');
+Route::get('users/mypage/edit', 'UserController@edit')->name('mypage.edit');
+Route::get('users/mypage/address/edit', 'UserController@edit_address')->name('mypage.edit_address');
+Route::put('users/mypage', 'UserController@update')->name('mypage.update');
+
 Route::post('products/{product}/reviews', 'ReviewController@store');
 
 // お気に入り登録を行うアクションへのルーティング
