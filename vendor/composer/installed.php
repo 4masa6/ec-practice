a7020e08ffa16ad5ae68d339888395f53bba6321
mogata <70381096+4masa6@users.noreply.github.com>
2022-02-06 23:08:59 +0900

    72_決済機能を実装（１）
    - ライブラリをインストール
    -- sudo yum install -y php-gd php-zip
    -- composer require maatwebsite/excel --with-all-dependencies
    -- composer require payjp/payjp-php
    - Pay.jpに登録し、テスト用秘密鍵・公開鍵を.envに記述
    - 決済トークン用のカラムを追加

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '56099f1011d39916925bd8b5724f1b5bface9187',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '56099f1011d39916925bd8b5724f1b5bface9187',
+            'dev_requirement' => false,
+        ),
+        'payjp/payjp-php' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../payjp/payjp-php',
+            'aliases' => array(),
+            'reference' => '41f149620e403a1c6e70b174d6652145b3e3a8d9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
