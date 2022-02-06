a9e694629acefada4eafd2044b9e78c15fac2f16
mogata <70381096+4masa6@users.noreply.github.com>
2022-02-06 16:05:56 +0900

    69_管理機能を実装（９）受注一覧画面を実装
    - コントローラを作成
    -- php artisan make:controller Dashboard/OrderController
    - ShoppingCartモデルにgetOrders()メソッド作成
    - ビューを作成
    -- resources/views/dashboard/orders/index.blade.php
    - ルーティング作成
    - 受注一覧画面へのリンク作成

---
+++ src/app/Http/Controllers/Dashboard/OrderController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Pagination\LengthAwarePaginator;
+use App\ShoppingCart;
+
+class OrderController extends Controller
+{
+    public function index(Request $request)
+    {
+        $page = $request->page != null ? $request->page : 1; // リクエストにページが含まれていれば使用し、無ければ1
+        $code = $request->code != null ? $request->code : ""; // リクエストにコードが含まれていれば使用し、無ければ空
+        $sort = $request->sort;
+        $orders = ShoppingCart::getOrders($code);
+        $total = count($orders);
+        $orders = new LengthAwarePaginator(array_slice($orders, ($page - 1) * 15, 15), $total, 15, $page, array('path' => $request->url()));
+
+        return view('dashboard.orders.index', compact('orders', 'total', 'sort', 'code'));
+    }
+}
